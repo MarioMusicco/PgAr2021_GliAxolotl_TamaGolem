9cc723189f51e9732f4df0743003e6fe99c5c2b4
mario.musicco <mario.musicco@gmail.com>
2021-05-02 11:45:03 +0200

    enum degli elementi

---
+++ src/it/unibs/pga/tamagolem/Elementi.java
@@ -0,0 +1,6 @@
+package it.unibs.pga.tamagolem;
+
+public enum Elementi {
+    Montagna, Tuono, Marea, Lava, Tempesta, Ombra, Metallo, Cristallo, Nova, Etere
+    //Vinter, Tetofonta, Robb, Lange, Ila, Doich, Massi, Enrico, ..., ...
+}
